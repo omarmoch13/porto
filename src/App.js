269f5ee6607c8e17a02f6ee628441d442b2a672c
omar <muhammadjendral12@gmail.com>
2022-01-24 22:48:50 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Bottombar from "./component/bottom/Botbar";
-import Topbar from "./component/topbar/Topbar";
+
 import Intro from "./component/intro/Intro";
 import Works from "./component/works/Works";
 import Skill from "./component/skill/Skill";
@@ -26,19 +25,6 @@ function App() {
         <Route exact path="/portofolio" element={<Portofolio />} />
       </Routes>
     </Router>
-
-    // <div className="App">
-    //   <Topbar />
-    //   <div className="sections">
-    //     <Intro />
-    //     <Portofolio />
-    //     <Works />
-    //     <Skill />
-    //   </div>
-    //   <div className="bottombar">
-    //     <Bottombar />
-    //   </div>
-    // </div>
   );
 }
 
