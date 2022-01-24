6e9534da4ebdafcb42642937bbe3a646ebb7f3e6
omar <muhammadjendral12@gmail.com>
2022-01-24 23:00:56 +0700

    sdaa

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import "./App.css";
 
-// import Intro from "./component/intro/Intro";
-// import Works from "./component/works/Works";
-// import Skill from "./component/skill/Skill";
-// import Portofolio from "./component/portofolio/Portofolio";
 import Fullpage from "./component/Fullpage/Fullpage";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Fullpage />} />
-        {/* <Route exact path="/intro" element={<Intro />} />
-        <Route exact path="/works" element={<Works />} />
-        <Route exact path="/skill" element={<Skill />} />
-        <Route exact path="/portofolio" element={<Portofolio />} /> */}
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -10,18 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-// const express = require("express");
-// const cors = require("cors");
 
-// const clickme = express();
-// clickme.use(cors());
-
-// clickme.get("/", (req, res) => {
-//   res.clickme(pdf);
-// });
-// clickme.listen(4000);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
